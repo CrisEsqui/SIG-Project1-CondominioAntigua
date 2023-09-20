9eb91580147916b6c80f02f8ffc07382c7adf66d
geiisel <montoyageisel@estudiantec.cr>
2023-09-19 21:27:43 -0600

    Add Example 1 React Leaflet

---
+++ src/components/examples/Example1.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { MapContainer, TileLayer } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import './SimpleMap.css'
+
+const SimpleMap = () => {
+
+    return (
+    <MapContainer 
+      center={[9.83827, -83.94227]} 
+      zoom={13} 
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker 
+        position={[9.83827, -83.94227]}
+      >
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+    );
+
+}
+
+export default SimpleMap;
